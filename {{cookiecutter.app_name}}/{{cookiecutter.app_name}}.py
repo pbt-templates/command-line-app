6c58dd98ec62415cd5ddd15db82743406c34612d
Mariano Guerra <luismarianoguerra@gmail.com>
2014-05-03 23:37:34 +0200

    make age a required argument

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}.py
@@ -11,15 +11,10 @@ def make_parser():
     parser = argparse.ArgumentParser(prog=APP_NAME,
             description=APP_DESCRIPTION, epilog=HELP_EPILOG)
 
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                               help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-                               const=sum, default=max,
-                               help='sum the integers (default: find the max)')
     parser.add_argument('-t', '--true', action='store_const', const=True)
     parser.add_argument('-f', '--false', action='store_const', const=False)
     parser.add_argument('--verbose', '-v', action='count')
-    parser.add_argument('age', type=int)
+    parser.add_argument('-a', '--age', type=int, required=True)
     parser.add_argument('move', choices=['rock', 'paper', 'scissors'])
 
     parser.add_argument('--version', action='version',
